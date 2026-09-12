2216341ed65d052d2bf0589804738848618d882f
Nageshwar Pandey <nageshwarpandey198@gmail.com>
2026-09-12 22:01:14 +0530

    Refactor StudentService method parameter names
    Refactor method parameters for createNewStudent and updateStudent to use 'request' instead of 'addStudentReqDto'.

--- spring-boot/curd/src/main/java/com/lokayoo/learn/service/StudentService.java
+++ spring-boot/curd/src/main/java/com/lokayoo/learn/service/StudentService.java
@@ -6,13 +6,20 @@ import java.util.Map;
 import com.lokayoo.learn.dto.AddStudentReqDto;
 import com.lokayoo.learn.dto.StudentDto;
 
+/**
+ * Defines business operations for student management.
+ */
 public interface StudentService {
 
     List<StudentDto> getAllStudents();
+
     StudentDto getStudentById(Long id);
-    StudentDto createNewStudent(AddStudentReqDto addStudentReqDto);
+
+    StudentDto createNewStudent(AddStudentReqDto request);
+
     void deleteStudentById(Long id);
-    StudentDto updateStudent(Long id, AddStudentReqDto addStudentReqDto);
+
+    StudentDto updateStudent(Long id, AddStudentReqDto request);
+
     StudentDto patchStudent(Long id, Map<String, Object> updates);
-    
 }
