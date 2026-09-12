fd75452c3fd4f355040cf1ba50f72bdcfac5fdde
Nageshwar Pandey <nageshwarpandey198@gmail.com>
2026-09-12 22:02:35 +0530

    Enhance StudentRepository with Javadoc comments
    Added Javadoc comments to the StudentRepository interface to describe its purpose and standard operations provided by JpaRepository.

--- spring-boot/curd/src/main/java/com/lokayoo/learn/repository/StudentRepository.java
+++ spring-boot/curd/src/main/java/com/lokayoo/learn/repository/StudentRepository.java
@@ -1,11 +1,23 @@
 package com.lokayoo.learn.repository;
 
-import com.lokayoo.learn.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.lokayoo.learn.entity.Student;
 
-@Repository 
+/**
+ * Repository interface for performing database operations on Student entities.
+ *
+ * <p>JpaRepository provides standard operations such as:</p>
+ * <ul>
+ *     <li>findAll()</li>
+ *     <li>findById()</li>
+ *     <li>save()</li>
+ *     <li>deleteById()</li>
+ *     <li>existsById()</li>
+ * </ul>
+ */
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    
+    // Custom student database queries can be added here when required.
 }
